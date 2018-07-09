80535a03217bca88f8abbe9e0797d66fde545bf6
vijit101 <40598861+vijit101@users.noreply.github.com>
2018-07-09 23:08:14 +0530

    First upload of Rock it!! Thrust problem
    I am facing a problem with addrelativeforce()  i don't know why it adds force to z axis instead of y and gives me an abnormal behaviour i want rocket to go up but its do not. i tried it with translation of position and its works awesome

---
+++ Assets/Scripts/rocketactor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocketactor : MonoBehaviour {
+    [SerializeField] float speed = 1;
+    Rigidbody myrb;
+
+	// Use this for initialization
+	void Start () {
+        myrb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        thrust();
+	}
+    private void thrust()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            myrb.AddRelativeForce(new Vector3(0,1,0)* speed,ForceMode.Force);
+            Debug.Log("inside thrust");
+        }
+    }
+
+}
